52c8798e8a96a2e7092e5c75651282b537f79f23
Milo Casagrande <milo@milo.name>
2012-03-22 22:31:48 +0100

    Added license header, fixed default port

--- src/com/customagent/CustomJMXAgent.java
+++ src/com/customagent/CustomJMXAgent.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2012 Milo Casagrande milo@milo.name
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
 package com.customagent;
 
 import java.io.IOException;
@@ -23,7 +38,7 @@ import javax.rmi.ssl.SslRMIServerSocketFactory;
  */
 public class CustomJMXAgent {
     // Default port for the RMI Server
-    private static final String DEFAULT_RMI_PORT = "8080";
+    private static final String DEFAULT_RMI_PORT = "7199";
 
     private CustomJMXAgent() {
         // Private and empty
